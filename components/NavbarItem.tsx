898b9bc334c8cf42b162f743b023b5569a491a2e
Subodh <sohonisubodh@gmail.com>
2024-01-30 22:48:20 +0530

    commit

--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -9,11 +9,7 @@ interface NavbarItemProps {
 
 const NavbarItem: React.FC<NavbarItemProps> = ({ label, active}) => {
   return (
-    <div className={active ? 'text-white cursor-default' : 'text-gray-200 hover:text-gray-300 cursor-pointer transition'} >
-
-      {label}
-      
-    </div>
+    
   )
 }
 
